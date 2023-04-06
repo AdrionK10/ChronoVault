f0a598ffbc335f86c56fa082e39e7750ea1a2c9a
Adrion Katon <HuxleysFather@gmail.com>
2023-04-06 15:22:12 -0700

    Initial commit

---
+++ ChronoVault/BackupManager.cs
@@ -0,0 +1,202 @@
+﻿using System.Globalization;
+
+namespace ChronoVault
+{
+    public class BackupManager
+    {
+        private string source;
+        private string backupRoot;
+        private string[] fileTypes;
+        private int maxBackups;
+        private int interval;
+        private bool isPaused;
+        public bool BackupModifiedOnly { get; private set; }
+        public DateTime NextBackupTime { get; private set; }
+
+        public DateTime LastBackupTime { get; private set; }
+        public bool IsPaused
+        {
+            get { return isPaused; }
+        }
+        
+        private AutoResetEvent pauseHandle = new AutoResetEvent(false);
+
+        public BackupManager(string source, string backupRoot, string[] fileTypes, int maxBackups, int interval,
+            bool backupModifiedOnly, bool copyAllOnStartup)
+        {
+            this.source = source;
+            this.backupRoot = backupRoot;
+            this.fileTypes = fileTypes;
+            this.maxBackups = maxBackups;
+            this.interval = interval;
+            isPaused = false;
+            BackupModifiedOnly = backupModifiedOnly;
+            Directory.CreateDirectory(backupRoot);
+            if (!copyAllOnStartup)
+                LastBackupTime = DateTime.Now.AddSeconds(-interval);
+        }
+
+        public void StartBackupLoop()
+        {
+            var currentBackupNum = GetMostRecentIndex(backupRoot);
+            
+            if (currentBackupNum > maxBackups)
+                currentBackupNum = maxBackups;
+            else
+                currentBackupNum++;
+            
+            while (true)
+            {
+                if (isPaused)
+                {
+                    pauseHandle.WaitOne();
+                }
+
+                var now = DateTime.Now;
+                NextBackupTime = now.AddSeconds(interval);
+
+                var currentBackup = FindFolderWithPrefix(backupRoot, $"{currentBackupNum}_");
+                if (currentBackup != null)
+                {
+                    Directory.Delete(currentBackup, true); // Delete the old backup folder
+                }
+
+                currentBackup = CreateBackupFolder(backupRoot, currentBackupNum); // Create a new backup folder with an updated timestamp
+
+                int copiedFilesCount = CopyFiles(source, currentBackup, fileTypes);
+                LastBackupTime = DateTime.Now;
+
+                if (copiedFilesCount > 0)
+                {
+                    // Progress the index only if files were copied
+                    currentBackupNum = currentBackupNum % maxBackups + 1;
+                }
+                else
+                {
+                    // Delete the empty backup folder
+                    Directory.Delete(currentBackup, true);
+                }
+
+                var timeToSleep = NextBackupTime - DateTime.Now;
+                if (timeToSleep.TotalMilliseconds > 0)
+                {
+                    Thread.Sleep(timeToSleep);
+                }
+            }
+        }
+        
+        private int GetMostRecentIndex(string root)
+        {
+            int maxIndex = 0;
+
+            foreach (var folder in Directory.GetDirectories(root))
+            {
+                var folderName = Path.GetFileName(folder);
+                if (int.TryParse(folderName.Split('_')[0], out var currentIndex))
+                {
+                    if (currentIndex > maxIndex)
+                        maxIndex = currentIndex;
+                }
+            }
+
+            if (maxIndex >= maxBackups)
+            {
+                maxIndex = maxBackups;
+            }
+
+            Console.WriteLine(maxIndex == 0 ? "No previous backups found" : $"Found backups up to index {maxIndex}");
+
+            Console.WriteLine($"Resuming at index {maxIndex % maxBackups + 1}");
+            return maxIndex;
+        }
+        
+        public void PauseBackup()
+        {
+            isPaused = true;
+        }
+
+        public void ResumeBackup()
+        {
+            isPaused = false;
+            pauseHandle.Set();
+        }
+
+        private string? FindFolderWithPrefix(string root, string prefix)
+        {
+            return Directory.GetDirectories(root).FirstOrDefault(folder => Path.GetFileName(folder).StartsWith(prefix));
+        }
+
+        private string CreateBackupFolder(string root, int index)
+        {
+            var folderName = $"{index}_{DateTime.Now:HH-mm_dd-MM-yyyy}";
+            var folderPath = Path.Combine(root, folderName);
+            Directory.CreateDirectory(folderPath);
+            return folderPath;
+        }
+
+        private int CopyFiles(string source, string destination, string[] fileTypes, bool restoring = false)
+        {
+            var copiedFilesCount = 0;
+
+            foreach (var fileType in fileTypes)
+            {
+                foreach (var file in Directory.GetFiles(source, fileType))
+                {
+                    var fileInfo = new FileInfo(file);
+                    var destinationFile = Path.Combine(destination, fileInfo.Name);
+
+                    if (BackupModifiedOnly && !restoring)
+                    {
+                        if (fileInfo.LastWriteTime <= LastBackupTime)
+                        {
+                            // Skip the file if it hasn't been modified since the last backup
+                            continue;
+                        }
+                    }
+
+                    File.Copy(file, destinationFile, true);
+                    copiedFilesCount++;
+                }
+            }
+
+            if (!restoring)
+            {
+                var changedString = BackupModifiedOnly ? "modified" : "";
+
+                var folderName = Path.GetFileName(destination);
+
+                var splitFolderName = folderName.Split('_');
+
+                var index = splitFolderName[0].Replace("_", "");
+
+// Combine the time and date parts from the folder name
+                var timestamp = $"{splitFolderName[1]}_{splitFolderName[2]}";
+
+// Parse the timestamp using the correct format
+                var parsedTime = DateTime.ParseExact(timestamp, "HH-mm_dd-MM-yyyy", CultureInfo.InvariantCulture);
+
+// Format the parsed time as desired
+                var outputTime = parsedTime.ToString("hh:mm tt dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+                if (copiedFilesCount > 0)
+                    Console.WriteLine(
+                        $"{copiedFilesCount} {changedString} files backed up to index {index} at {outputTime}");
+            }
+
+            return copiedFilesCount;
+        }
+
+        public void RestoreBackup(string backupFolder)
+        {
+            if (Directory.Exists(backupFolder))
+            {
+                CopyFiles(backupFolder, source, fileTypes, restoring: true);
+                Console.WriteLine($"Backup restored from {backupFolder}");
+            }
+            else
+            {
+                Console.WriteLine($"Backup folder {backupFolder} not found");
+            }
+        }
+    }
+}
---
+++ ChronoVault/Program.cs
@@ -0,0 +1,141 @@
+﻿using System.Globalization;
+
+namespace ChronoVault
+{
+    class Program
+    {
+        private static void Main()
+        {
+            var config = ReadConfig("config.ini");
+
+            var source = config["source"];
+            var backupRoot = config["backupRoot"];
+            var fileTypes = config["fileTypes"].Split(',');
+            var maxBackups = Convert.ToInt32(config["maxBackups"]);
+            var seconds = Convert.ToInt32(config["secondsBetweenBackups"]);
+            var modifiedOnly = Convert.ToBoolean(config["backupModifiedOnly"]);
+            var copyAllOnStartup = Convert.ToBoolean(config["copyAllOnStartup"]);
+
+            var backupManager = new BackupManager(source, backupRoot, fileTypes, maxBackups, seconds, modifiedOnly,
+                copyAllOnStartup);
+
+            // Start the backup loop in a separate thread
+            var backupThread = new Thread(backupManager.StartBackupLoop);
+            var timeLoopSpan = TimeSpan.FromSeconds(seconds * maxBackups);
+            backupThread.Start();
+
+            Console.WriteLine("Configuration loaded:");
+            Console.WriteLine();
+            Console.WriteLine("Source: " + source
+                              + Environment.NewLine + "Backup root: " + backupRoot
+                              + Environment.NewLine + "File types: " + config["fileTypes"]
+                              + Environment.NewLine + "Max backups: " + maxBackups
+                              + Environment.NewLine + "Seconds between backups: " + seconds
+                              + Environment.NewLine + "Backup modified only: " + modifiedOnly
+                              + Environment.NewLine + "Copy all on startup: " + copyAllOnStartup);
+            Console.WriteLine();
+            Console.WriteLine("Backup Span: ("
+                              + timeLoopSpan.Days + ") Days ("
+                              + timeLoopSpan.Hours + ") Hours ("
+                              + timeLoopSpan.Minutes + ") Minutes ("
+                              + timeLoopSpan.Seconds + ") Seconds ");
+            
+            Console.WriteLine();
+            Console.WriteLine("ChronoVault is running in the background");
+            Console.WriteLine();
+            Console.WriteLine("Press 'P' to pause or resume the backup.");
+            Console.WriteLine("Press 'R' to restore a backup.");
+            Console.WriteLine("Press 'Q' to quit.");
+            Console.WriteLine("");
+
+            while (true)
+            {
+                var keyInfo = Console.ReadKey(true);
+
+                if (keyInfo.Key == ConsoleKey.P)
+                {
+                    if (backupManager.IsPaused)
+                    {
+                        Console.WriteLine("Resumed backup >>");
+                        backupManager.ResumeBackup();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Paused Backup.");
+                        backupManager.PauseBackup();
+                    }
+                }
+                else if (keyInfo.Key == ConsoleKey.R)
+                {
+                    Console.WriteLine("\nAvailable backups:");
+                    var backupFolders = Directory.GetDirectories(backupRoot);
+                    var index = 1;
+
+                    foreach (var folder in backupFolders)
+                    {
+                        var folderName = Path.GetFileName(folder);
+
+                        var splitFolderName = folderName.Split('_');
+
+// Combine the time and date parts from the folder name
+                        var timestamp = $"{splitFolderName[1]}_{splitFolderName[2]}";
+
+// Parse the timestamp using the correct format
+                        var parsedTime = DateTime.ParseExact(timestamp, "HH-mm_dd-MM-yyyy", CultureInfo.InvariantCulture);
+
+// Format the parsed time as desired
+                        var outputTime = parsedTime.ToString("hh:mm tt dd/MM/yyyy", CultureInfo.InvariantCulture);
+                        
+                        Console.WriteLine($"{index}: {outputTime}");
+                        
+                        index++;
+                    }
+
+                    Console.WriteLine("Enter the number of the backup you want to restore, or type 'C' to cancel:");
+                    var input = Console.ReadLine();
+                    if (string.IsNullOrEmpty(input)) continue;
+                    if (int.TryParse(input, out var selectedIndex) && selectedIndex > 0 &&
+                        selectedIndex <= backupFolders.Length)
+                    {
+                        var selectedBackup = backupFolders[selectedIndex - 1];
+                        Console.WriteLine($"Restoring backup from {Path.GetFileName(selectedBackup)}...");
+                        backupManager.RestoreBackup(selectedBackup);
+                    }
+                    else if (input.Equals("C", StringComparison.OrdinalIgnoreCase))
+                    {
+                        Console.WriteLine("Restore operation canceled.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid selection. Please try again.");
+                    }
+
+                    Console.WriteLine("\nPress 'P' to pause or resume the backup.");
+                    Console.WriteLine("Press 'R' to restore a backup.");
+                    Console.WriteLine("Press 'Q' to quit.");
+                }
+                else if (keyInfo.Key == ConsoleKey.Q)
+                {
+                    break;
+                }
+            }
+        }
+
+        private static Dictionary<string, string> ReadConfig(string fileName)
+        {
+            var config = new Dictionary<string, string>();
+
+            using var sr = new StreamReader(fileName);
+
+            while (sr.ReadLine() is { } line)
+            {
+                var keyValue = line.Split('=');
+
+                if (keyValue.Length == 2)
+                    config[keyValue[0].Trim()] = keyValue[1].Trim();
+            }
+
+            return config;
+        }
+    }
+}
